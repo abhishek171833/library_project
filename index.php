c576010ec49f0edd3532a2dc9a56c89cbbcd0c98
abhi <programwithabhi@gmail.com>
2022-12-26 23:07:30 +0530

    signup and login functionality added

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -13,8 +14,85 @@
         <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         <link href="css/styles.css" rel="stylesheet" />
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     </head>
+    <?php
+        if(isset($_POST['lemail'])){
+        require('./db/conn.php');
+        $res=mysqli_query($db,"SELECT * FROM `student` WHERE Email='$_POST[lemail]' && Password='$_POST[lpassword]';");
+        $count=mysqli_num_rows($res);
+
+        if($count){
+            $_SESSION['login_user'] = $_POST['lemail'];?>
+            <script>
+                setTimeout(() => {
+                    swal("Success!", "You Logged In Successfully!", "success")
+                }, 1000);
+                // window.location="index.php"
+            </script>
+            
+            <?php }
+        else{
+            ?>
+            <script>
+                setTimeout(() => {
+                    swal("Error!", "Email Or Password Does Not Match!", "error");
+                }, 1000);
+            </script>
+        <?php }
+        }
+
+
+
+        if(isset($_POST['name'])){
+            require("./db/conn.php");
+
+            if($_POST['password'] != $_POST['cpassword']):?>
+                <script>
+                    setTimeout(() => {
+                        swal("Warning!", "Password And Confirm Password Does Not Match!", "warning");
+                    }, 1000);
+                </script>
+
+            <?php 
+            $sql = "SELECT Email FROM `student` WHERE Email='$_POST[email]';";
+            print_r($db);
+            die;
+              $res=mysqli_query($db,$sql);
+              $count=mysqli_num_rows($res);
+
+            elseif($count):?>
+                <script>
+                    setTimeout(() => {
+                        swal("Warning!", "User With This Email Already Exists!", "warning");
+                    }, 1000);
+                </script>
+            <?php 
+
+            $res=mysqli_query($db,"SELECT phone FROM `student` WHERE Contact='$_POST[phone]';");
+            $count=mysqli_num_rows($res);
+
+            elseif($count): ?>
+                <script>
+                    setTimeout(() => {
+                        swal("Warning!", "User With This Phone Number Already Exists!", "warning");
+                    }, 1000);
+                </script>
+
+            <?php else:
+                    mysqli_query($db,"INSERT INTO `STUDENT` VALUES('$_POST[name]', '$_POST[last_name]', '$_POST[username]', '$_POST[password]', '$_POST[roll_no]', '$_POST[phone]', '$_POST[email]');");?>
+                    <script>
+                        setTimeout(() => {
+                            swal("Success!", "Sign Up Successfully Now You Can Log In!", "success");
+                        }, 1000);
+                    </script>
+                <?php
+              endif;
+            }
+        ?>
     <body id="page-top">
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
@@ -29,10 +107,96 @@
                         <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
                         <li class="nav-item"><a class="nav-link" href="#projects">Projects</a></li>
                         <li class="nav-item"><a class="nav-link" href="#signup">Contact</a></li>
+                        <?php
+                            if(isset($_SESSION['login_user'])){?>
+                        <a class="nav-link" style="cursor:pointer;" href="logout.php">Logout</a>
+                        <?php }
+                         else { ?>
+                            <a class="nav-link" style="cursor:pointer;" data-bs-toggle="modal" data-bs-target="#loginModal">Log In</a>
+                        <a class="nav-link" style="cursor:pointer;" data-bs-toggle="modal" data-bs-target="#signupModal">Sign Up</a>
+                        <?php }?>
                     </ul>
                 </div>
             </div>
         </nav>
+        <!-- Modal -->
+        <div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form action="./index.php" method="post">
+                        <div class="mb-3">
+                            <label for="lemail" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="lemail" aria-describedby="emailHelp" name="lemail" required>
+                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="lpassword" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="lpassword" name="lpassword" required>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Log In</button>
+                        </div>
+                    </form>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="signupModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form class="row" action="./index.php" method="post">
+                        <div class="mb-3 col-md-6">
+                            <label for="name" class="form-label">First Name</label>
+                            <input type="text" class="form-control" id="name" aria-describedby="emailHelp" name="name" required>
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label for="last_name" class="form-label">Last Name</label>
+                            <input type="text" class="form-control" id="last_name" name="last_name" required>
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label for="username" class="form-label">Username</label>
+                            <input type="text" class="form-control" id="last_name" name="username" required>
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label for="email" class="form-label">Email Address</label>
+                            <input type="email" class="form-control" id="email" name="email" required>
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label for="phone" class="form-label">Phone</label>
+                            <input type="number" class="form-control" id="email" name="phone" required>
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label for="roll_no" class="form-label">Roll No.</label>
+                            <input type="number" class="form-control" id="roll_no" name="roll_no" required>
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="password" name="password" required>
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label for="cpassword" class="form-label">Confirm Password</label>
+                            <input type="password" class="form-control" id="cpassword" name="cpassword" required>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Sign Up</button>
+                        </div>
+                    </form>
+                </div>
+                </div>
+            </div>
+        </div>
         <!-- Masthead-->
         <header class="masthead">
             <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
@@ -198,10 +362,5 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
         <script src="js/scripts.js"></script>
-        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
-        <!-- * *                               SB Forms JS                               * *-->
-        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
-        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
-        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
     </body>
 </html>
