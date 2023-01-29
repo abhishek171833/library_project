0ccd4717ed14ec4546c89f3d06bab3becb80bb23
abhi <programwithabhi@gmail.com>
2023-01-29 21:09:49 +0530

    order book functionality added

--- books.php
+++ books.php
@@ -17,11 +17,62 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="/resources/demos/style.css">
+        <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+        <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+        <script>
+            $( function() {
+                $("#datepicker" ).datepicker({ minDate: 0});
+                $("#datepicker2" ).datepicker({ minDate:0 });
+                // $("#datepicker2" ).datepicker({ minDate:'+4d' });
+                // $("#datepicker" ).datepicker();
+            } );
+        </script>
     </head>
     <?php 
     session_start();
     if(isset($_SESSION['login_user'])){ ?>
     <body>
+    <div class="modal fade" id="bookModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Order This Book</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <!-- <form action="./index.php" method="post"> -->
+                    <div class="mb-3">
+                        <label for="lemail" class="form-label">Book Name</label>
+                        <input type="email" class="form-control" id="lemail" aria-describedby="emailHelp" name="book" value="Bhagvatgita" disabled>
+                    </div>
+                    
+                    <div class="row">
+                        <div class="col-md-6 mb-2">
+                            <div class="input-group-prepend mb-1">
+                                <span class="input-group-text" id="basic-addon1">From</span>
+                            </div>
+                            <input class="form-control" id="datepicker">
+                        </div>
+    
+                        <div class="col-md-6 mb-2">
+                            <div class="input-group-prepend mb-1">
+                                <span class="input-group-text" id="basic-addon1">To</span>
+                            </div>
+                            <input class="form-control" id="datepicker2">
+                        </div>
+                    </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button id="order-book" class="btn btn-primary">Order Book</button>
+                    </div>
+                <!-- </form> -->
+            </div>
+            </div>
+        </div>
+    </div>
     <nav class="navbar navbar-expand-lg navbar-light fixed-top bg-secondary">
         <div class="container px-4 px-lg-5">
             <a class="navbar-brand text-warning" href="index.php">Library Management</a>
@@ -66,7 +117,7 @@
                                 <p class="card-text">Book Name</p>
                                 <div class="d-flex justify-content-between align-items-center">
                                     <div class="btn-group">
-                                        <button type="button" class="btn btn-sm btn-outline-secondary">Oder book</button>
+                                        <button  data-bs-toggle="modal" data-bs-target="#bookModal" type="button" class="btn btn-sm btn-outline-secondary">Oder book</button>
                                     </div>
                                     <small class="text-muted">9 mins</small>
                                 </div>
@@ -230,3 +281,10 @@
       
 </script>
 <?php }?>
+<script>
+    let order_book = document.getElementById("order-book")
+    order_book.addEventListener("click",function(){
+        let val = document.getElementById("datepicker")
+        console.log(parseInt(val.value.substr(3,6)))
+    })
+</script>
